5782d08cabf7e668f8ae209b03ba8008db41b6be
sohaib <rajashahzaibabdullah8467@gmail.com>
2026-08-27 09:08:22 +0500

    Add all the desired content in menu page
    - added further categories and dishes in the addToMenu function to
      include them in the menu page.

--- src/index.js
+++ src/index.js
@@ -84,39 +84,152 @@ const addToMenu = function () {
     const title = document.createElement("div");
     title.classList.add("title");
     title.textContent = "Menu";
-
-    const category = document.createElement("div");
-    category.classList.add("category");
-    
-
-    const categoryName = document.createElement("div");
-    categoryName.classList.add("category-name");
-    categoryName.textContent = "Beverages";
-
-    const dishItem = document.createElement("div");
-    dishItem.classList.add("dish-item");
-    
-
-    const dishName = document.createElement("div");
-    dishName.classList.add("dish-name");
-    dishName.textContent = "Honey Tea";
-
-    const description = document.createElement("div");
-    description.classList.add("description");
-    description.textContent = "A warm, Sweat tea made with the highest quality honey and a bit of lemon to start your day off right!";
-
-    const prSpan = document.createElement("span");
-    prSpan.textContent = "$2";
-
-
     menu.appendChild(title);
-    category.appendChild(categoryName);
-    dishItem.appendChild(dishName);
-    description.appendChild(prSpan);
-    dishItem.appendChild(description);
-    category.appendChild(dishItem);
 
-    menu.appendChild(category);
+    const categoryBeverages = document.createElement("div");
+    categoryBeverages.classList.add("category");
+
+    const categoryBeveragesName = document.createElement("div");
+    categoryBeveragesName.classList.add("category-name");
+    categoryBeveragesName.textContent = "Beverages";
+
+    const dishItemHoneyTea = document.createElement("div");
+    dishItemHoneyTea.classList.add("dish-item");
+
+    const dishNameHoneyTea = document.createElement("div");
+    dishNameHoneyTea.classList.add("dish-name");
+    dishNameHoneyTea.textContent = "Honey Tea";
+
+    const descriptionHoneyTea = document.createElement("div");
+    descriptionHoneyTea.classList.add("description");
+    descriptionHoneyTea.textContent = "A warm, Sweat tea made with the highest quality honey and a bit of lemon to start your day off right!";
+
+    const prSpanHoneyTea = document.createElement("span");
+    prSpanHoneyTea.textContent = "$2";
+
+    categoryBeverages.appendChild(categoryBeveragesName);
+    dishItemHoneyTea.appendChild(dishNameHoneyTea);
+    descriptionHoneyTea.appendChild(prSpanHoneyTea);
+    dishItemHoneyTea.appendChild(descriptionHoneyTea);
+    categoryBeverages.appendChild(dishItemHoneyTea);
+
+    const dishItemBearyTea = document.createElement("div");
+    dishItemBearyTea.classList.add("dish-item");
+
+    const dishNameBearyTea = document.createElement("div");
+    dishNameBearyTea.classList.add("dish-name");
+    dishNameBearyTea.textContent = "Beary Tea";
+
+    const descriptionBearyTea = document.createElement("div");
+    descriptionBearyTea.classList.add("description");
+    descriptionBearyTea.textContent = "A comforting, almost filling, tea that is infused with the flavors of several kinds of berries. Best served cold, but can be served hot on request.";
+
+    const prSpanBearyTea = document.createElement("span");
+    prSpanBearyTea.textContent = "$3";
+
+    dishItemBearyTea.appendChild(dishNameBearyTea);
+    descriptionBearyTea.appendChild(prSpanBearyTea);
+    dishItemBearyTea.appendChild(descriptionBearyTea);
+    categoryBeverages.appendChild(dishItemBearyTea);
+
+
+    menu.appendChild(categoryBeverages);
+
+    const categorySides = document.createElement("div");
+    categorySides.classList.add("category");
+
+    const categorySidesName = document.createElement("div");
+    categorySidesName.classList.add("category-name");
+    categorySidesName.textContent = "Sides";
+
+    const dishItemToastAndJam = document.createElement("div");
+    dishItemToastAndJam.classList.add("dish-item");
+
+    const dishNameToastAndJam = document.createElement("div");
+    dishNameToastAndJam.classList.add("dish-name");
+    dishNameToastAndJam.textContent = "Toast & Jam";
+
+    const descriptionToastAndJam = document.createElement("div");
+    descriptionToastAndJam.classList.add("description");
+    descriptionToastAndJam.textContent = "A slice of Toast, your choice of bread, and our homemade blackberry or raspberry jam";
+
+    const prSpanToastAndJam = document.createElement("span");
+    prSpanToastAndJam.textContent = "$1";
+
+    categorySides.appendChild(categorySidesName);
+    dishItemToastAndJam.appendChild(dishNameToastAndJam);
+    descriptionToastAndJam.appendChild(prSpanToastAndJam);
+    dishItemToastAndJam.appendChild(descriptionToastAndJam);
+    categorySides.appendChild(dishItemToastAndJam);
+
+    const dishItemFreshFruit = document.createElement("div");
+    dishItemFreshFruit.classList.add("dish-item");
+
+    const dishNameFreshFruit = document.createElement("div");
+    dishNameFreshFruit.classList.add("dish-name");
+    dishNameFreshFruit.textContent = "Fresh Fruit";
+
+    const descriptionFreshFruit = document.createElement("div");
+    descriptionFreshFruit.classList.add("description");
+    descriptionFreshFruit.textContent = "A small bowl of fresh fruit, whatever we find at the market for the day";
+
+    const prSpanFreshFruit = document.createElement("span");
+    prSpanFreshFruit.textContent = "$3";
+
+    dishItemFreshFruit.appendChild(dishNameFreshFruit);
+    descriptionFreshFruit.appendChild(prSpanFreshFruit);
+    dishItemFreshFruit.appendChild(descriptionFreshFruit);
+    categorySides.appendChild(dishItemFreshFruit);
+
+    menu.appendChild(categorySides);
+
+    const categoryMainDishes = document.createElement("div");
+    categoryMainDishes.classList.add("category");
+
+    const categoryNameMainDishes = document.createElement("div");
+    categoryNameMainDishes.classList.add("category-name");
+    categoryNameMainDishes.textContent = "Main Dishes";
+
+    const dishItemPanCakes = document.createElement("div");
+    dishItemPanCakes.classList.add("dish-item");
+
+    const dishNamePanCakes = document.createElement("div");
+    dishNamePanCakes.classList.add("dish-name");
+    dishNamePanCakes.textContent = "Pan Cakes";
+
+    const descriptionPanCakes = document.createElement("div");
+    descriptionPanCakes.classList.add("description");
+    descriptionPanCakes.textContent = "A stack of homemade buttermilk pancakes, served with our locally sourced maple syrup.";
+
+    const prSpanPanCakes = document.createElement("span");
+    prSpanPanCakes.textContent = "$5";
+
+    descriptionPanCakes.appendChild(prSpanPanCakes);
+    categoryMainDishes.appendChild(categoryNameMainDishes);
+    dishItemPanCakes.appendChild(dishNamePanCakes);
+    dishItemPanCakes.appendChild(descriptionPanCakes);
+    categoryMainDishes.appendChild(dishItemPanCakes);
+
+    const dishItemFrenchToast = document.createElement("div");
+    dishItemFrenchToast.classList.add("dish-item");
+
+    const dishNameFrenchToast = document.createElement("div");
+    dishNameFrenchToast.classList.add("dish-name");
+    dishNameFrenchToast.textContent = "French Toast";
+
+    const descriptionFrenchToast = document.createElement("div");
+    descriptionFrenchToast.classList.add("description");
+    descriptionFrenchToast.textContent = "Two slices of the best French Toast you will ever eat, served with our locally sourced maple syrup";
+
+    const prSpanFrenchToast = document.createElement("span");
+    prSpanFrenchToast.textContent = "$5";
+
+    descriptionFrenchToast.appendChild(prSpanFrenchToast);
+    dishItemFrenchToast.appendChild(dishNameFrenchToast);
+    dishItemFrenchToast.appendChild(descriptionFrenchToast);
+    categoryMainDishes.appendChild(dishItemFrenchToast);
+
+    menu.appendChild(categoryMainDishes)
 
 }
 
